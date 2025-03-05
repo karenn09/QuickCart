d8e9636fb4354f991ef9c8fd64e1e680823af6bd
Karen <ogmaren@gmail.com>
2025-03-04 23:01:06 -0500

    committ

--- config/inngest.js
+++ config/inngest.js
@@ -1,5 +1,6 @@
 import { Inngest } from "inngest";
 import connectDb from "./db";
+import User from "../models/User";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "atelier-shop" });
